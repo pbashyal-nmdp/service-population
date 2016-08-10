703c494d2f64a2c3d29fd3e2155b3760731ac0a1
pbashyal-nmdp <pbashyal@nmdp.org>
2016-08-10 16:16:15 -0500

    Initial Import
    Initial Import of the Spring Boot Initializer with the following dependencies.
    Web×JPA×H2×MySQL×Actuator×Actuator Docs

---
+++ src/main/java/org/nmdp/population/ServicePopulationApplication.java
@@ -0,0 +1,12 @@
+package org.nmdp.population;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePopulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePopulationApplication.class, args);
+	}
+}
---
+++ src/test/java/org/nmdp/population/ServicePopulationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.nmdp.population;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicePopulationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
